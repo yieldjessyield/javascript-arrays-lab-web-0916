52c02bda3dfc40275e358b3ca198fb54e4c82c65
Jess <jess.harrelson0707@gmail.com>
2016-11-02 21:43:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+    kittens.pop()
+    return kittens
+
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var cats = [...kittens, name]
+  return cats
+}
+
+function prependKitten(name){
+  var cats = [name, ...kittens]
+  return cats
+}
+
+function removeLastKitten(){
+  var blerb = kittens.slice(0, kittens.length - 1)
+    return blerb
+}
+
+function removeFirstKitten(){
+  var blerb = kittens.slice(1, kittens.length)
+    return blerb
+}
